32150f9c25378d13c37e601f607246a1e55d7196
khock1 <45620184+khock1@users.noreply.github.com>
2018-12-07 12:53:47 +1000

    Update hysteresis_coral.m

--- hysteresis_coral.m
+++ hysteresis_coral.m
@@ -19,11 +19,11 @@ z = 0.4;%supplemental macroalgal mortality, from herbivores
 b = 0.4;%adult overgrowth
 o = 4;%herbivore habitat provisioning
 
-parameter_range= 0:0.02:0.4;
+parameter_range = 0:0.02:0.4;
 
 figure;hold
 for i=1:length(parameter_range)
-    z = parameter_range(i);%subsititute z here to perform stability analysis with a different paramter
+    z = parameter_range(i);%substitute z here to perform stability analysis with a different paramter
     R = -(C*r*(C + M - 1))/(a + n + C*r + M*s);%pre-solved coral recruitment equation dR = r*A*(1-R-A-M) - a*R - s*R*M - n*R to obtain R
     dA = a*R + g*C*(1-R-C-M) - b*C*M - m*C;%adult coral
     dM = s*M*(1-R-C-M) + s*R*M + b*C*M - h*M - (z*M*o*C)/(1+o*C);%macroalgae
@@ -31,9 +31,9 @@ for i=1:length(parameter_range)
     ss1 = vpasolve([0 == a*R + g*C*(1-R-C-M) - b*C*M - m*C,0==s*M*(1-R-C-M) + s*R*M + b*C*M - h*M - (z*M*o*C)/(1+o*C)],[C,M]);%obtain all numerical solutions for the ODEs
     for jj=1:length(ss1.C)%sequentially plot equilibria for a given range of parameter values
         if isreal(ss1.C(jj)) && isreal(ss1.M(jj))%only concerned with real equilibria
-            if all([ss1.C(jj) ss1.M(jj)]>=0 & [ss1.C(jj) ss1.M(jj)]<=1)%only concerned with nontrivial solutions, i.e. if proportion od coral and macroalgal cover is less than 1
-                jaceigv=eig(double(subs(J,[C M],[ss1.C(jj) ss1.M(jj)])));%substitute equilibrai into Jacobian to obtain eigenvalues
-                if any(jaceigv>=0)%eigenvalues >0 means equlilibrium is a saddle
+            if all([ss1.C(jj) ss1.M(jj)]>=0 & [ss1.C(jj) ss1.M(jj)]<=1)%only concerned with nontrivial solutions, i.e. if proportion of coral and macroalgal cover is less than 1
+                jacb_eigv = eig(double(subs(J,[C M],[ss1.C(jj) ss1.M(jj)])));%substitute equilibria into Jacobian to obtain eigenvalues
+                if any(jacb_eigv>=0)%Jacobian eigenvalues > 0 mean this equlilibrium is a saddle
                     scatter(parameter_range(i),ss1.C(jj),30,'filled','MarkerFaceColor','m');%if unstable
                 else
                     scatter(parameter_range(i),ss1.C(jj),30,'filled','MarkerFaceColor','r');%if stable
@@ -43,4 +43,4 @@ for i=1:length(parameter_range)
     end
 end
 ylabel('Adult Coral');
-xlabel('External Supply');
+xlabel('Grazing');
