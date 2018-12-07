8f20125c2c1bc3cc0684ad798b2e9bb26f60dc8d
khock1 <45620184+khock1@users.noreply.github.com>
2018-12-07 13:02:20 +1000

    Update Phase_plane_ODE_solver_coral_demo.m

--- Phase_plane_ODE_solver_coral_demo.m
+++ Phase_plane_ODE_solver_coral_demo.m
@@ -1,19 +1,19 @@
 % Author: Karlo Hock, University of Queensland.
-% Equations from Fabina et al. 2015, Ecological Applications, 25(6), 1534–1545
+% Equations from Fabina et al. 2015, Ecological Applications, 25(6), 1534â€“1545
 
 % Solve ODEs for coral-macroalgal dynamics, plot pahse plane portrait of the solutions
 % Solve for nullclines, then plot nullclines
 
 clear;
 %constrain parameter space to realistic values, ensuring that covers together cannot exceed 1
-x=0:0.05:1;
-y=0:0.05:1;
-spc=[0 0];
-axlim=0.8;%limit to parameter space to be xexplored, has to be <=1
+x = 0:0.05:1;
+y = 0:0.05:1;
+spc = [0 0];
+axlim = 0.8;%limit to parameter space to be xexplored, has to be <=1
 for i=1:size(x,2)
     for j=1:size(y,2)
-        if x(i)+y(j)<=axlim
-            spc=vertcat(spc,[x(i) y(j)]);
+        if x(i) + y(j) <= axlim
+            spc = vertcat(spc,[x(i) y(j)]);
         end
     end
 end
@@ -30,28 +30,27 @@ b = 0.4;%adult overgrowth
 o = 4;%herbivore habitat provisioning
 
 parameters = [a,s,n,g,b,m,h,z,o,r];
-figure;
-hold on;
+figure; hold on;
 
 %solve and draw solution for parameter space
 for ii = 1:length(spc)
-    options = odeset('NonNegative',1);
-    [T,Y] = ode45(@popdyn_coral,[0 200],[spc(ii,2),spc(ii,1)],options, parameters );%solve ODEs for parameter space
-    C = Y(:,1); % adult coral
-    M = Y(:,2); % macroalgae
-    phaseplane = plot(M,C,'Color','k'); % plot solutions
+    options = odeset('NonNegative', 1);
+    [T,Y] = ode45(@popdyn_coral, [0 200], [spc(ii,2), spc(ii,1)], options, parameters);%solve ODEs for parameter space
+    C = Y(:, 1); % adult coral
+    M = Y(:, 2); % macroalgae
+    phaseplane = plot(M, C, 'Color', 'k'); % plot solutions
 end
 
-title('Phase plane - Coral-macroalgal dynamics', 'FontSize',11)
+title('Phase plane - Coral-macroalgal dynamics', 'FontSize', 11)
 xlabel('Macroalgae');
 ylabel('Adult Coral');
 %perform stability analysis, draw stable and unstable equilbiria on a phase plane
 [stable_eq, unstable_eq] = stability_coral(parameters);
-for jj=1:size(unstable_eq,1)
-    scatter(unstable_eq(jj,1),unstable_eq(jj,2),140,'d','m','filled');
+for jj = 1:size(unstable_eq, 1)
+    scatter(unstable_eq(jj,1), unstable_eq(jj,2), 140, 'd', 'm', 'filled');
 end
-for jj=1:size(stable_eq,1)
-    scatter(stable_eq(jj,1),stable_eq(jj,2),140,'d','c','filled');%if stable
+for jj = 1:size(stable_eq,1)
+    scatter(stable_eq(jj,1), stable_eq(jj,2), 140, 'd', 'c', 'filled');%if stable
 end
 ylabel('Adult Coral');
 xlabel('Macroalgae');
@@ -60,16 +59,15 @@ set(gcf, 'PaperPositionMode', 'auto');
 print -depsc2 coral_phaseplane.eps
 
 %solve for coral and algal nullclines, draw them and draw the axes
-figure; hold;
-syms Cs Ms;
+figure; hold on;
 Rs = -(Cs*r*(Cs + Ms - 1))/(a + n + Cs*r + Ms*s);%pre-solved coral recruitment equation dR = r*A*(1-R-A-M) - a*R - s*R*M - n*R to obtain R
-coral_nullcline=solve(0 == a.*Rs + g.*Cs.*(1-Rs-Cs-Ms) - b.*Cs.*Ms - m.*Cs,Cs);
-algal_nullcline=solve(0 ==  s.*Ms.*(1-Rs-Cs-Ms) + s.*Rs.*Ms + b.*Cs*Ms - h.*Ms - (z.*Ms*o.*Cs)/(1+o.*Cs),Ms);
-plh=fplot(coral_nullcline(1),[0 1]);set(plh,'color','red','linewidth',2);
-plh=fplot(coral_nullcline(2),[0 1]);set(plh,'color','red','linewidth',2);
-plh=plot(zeros(1,11),0:0.1:1);set(plh,'color','blue','linewidth',2);
-plh=fplot(algal_nullcline(2),[0 1]);set(plh,'color','blue','linewidth',2);
-title('Nullclines - Coral-macroalgal dynamics', 'FontSize',11)
+coral_nullcline = solve(0 == a.*Rs + g.*Cs.*(1-Rs-Cs-Ms) - b.*Cs.*Ms - m.*Cs, Cs);
+algal_nullcline = solve(0 == s.*Ms.*(1-Rs-Cs-Ms) + s.*Rs.*Ms + b.*Cs*Ms - h.*Ms - (z.*Ms*o.*Cs)/(1+o.*Cs), Ms);
+cn1 = fplot(coral_nullcline(1), [0 1]); set(cn1, 'color', 'red', 'linewidth', 2);
+cn2 = fplot(coral_nullcline(2), [0 1]); set(cn2, 'color', 'red', 'linewidth', 2);
+an1 = plot(zeros(1,11), 0:0.1:1); set(an1, 'color', 'blue', 'linewidth', 2);
+an2 = fplot(algal_nullcline(2), [0 1]); set(an2, 'color', 'blue', 'linewidth', 2);
+title('Nullclines - Coral-macroalgal dynamics', 'FontSize', 11)
 xlabel('Macroalgae');
 ylabel('Adult Coral');
 axis([0 axlim 0 axlim])
